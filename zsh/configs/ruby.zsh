d7c2e13bc1fe0dcd5764d3a1930e17e62b534f44
Michael Herold <michael.j.herold@gmail.com>
2017-03-23 14:41:19 -0500

    [ruby] Stop using rvm at all

--- zsh/configs/ruby.zsh
+++ zsh/configs/ruby.zsh
@@ -1,10 +1,13 @@
 #!/usr/bin/env zsh
 
 # Chruby for when we have it
-[[ -f /usr/share/chruby/chruby.sh ]] && {
+[[ -d /usr/share/chruby ]] && {
   source /usr/share/chruby/chruby.sh
-  chruby ruby-2.3.1
+  source /usr/share/chruby/auto.sh
+}
+[[ -d /usr/local/share/chruby ]] && {
+  source /usr/local/share/chruby/chruby.sh
+  source /usr/local/share/chruby/auto.sh
 }
 
-# RVM for work laptop
-[[ -d ~/.rvm ]] && export PATH="${PATH}:${HOME}/.rvm/bin"
+which chruby >/dev/null && chruby ruby-2.4.0
