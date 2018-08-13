378ff07d108241dfc08dbcd92efab292637471d0
Michael Herold <opensource@michaeljherold.com>
2018-08-12 20:16:42 -0500

    [zsh] Add `asdf` support to the `PATH`

--- zsh/configs/post/00-path.zsh
+++ zsh/configs/post/00-path.zsh
@@ -1,5 +1,5 @@
 # ensure dotfiles bin directory is loaded first
-PATH="$HOME/bin:$HOME/.bin:/usr/local/sbin:$PATH"
+PATH="$HOME/bin:$HOME/.bin:$HOME/.asdf/bin:$HOME/.asdf/shims:/usr/local/sbin:$PATH"
 
 # mkdir .git/safe in the root of repositories you trust
 PATH=".git/safe/../../bin:$PATH"
