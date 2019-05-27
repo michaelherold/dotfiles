444e503b1a42d3a4c48ffb14fe0ec7f35fabab68
Michael Herold <opensource@michaeljherold.com>
2019-05-27 09:32:37 -0500

    [zsh] Ensure /usr/local/bin in PATH when present
    The way that Emacs was loading the PATH variable from my shell was
    missing `/usr/local/bin`, which is where Homebrew stores its binaries.
    As such, it wasn't seeing GPG and thus was unable to commit anything
    through Magit since I sign all of my commits.

--- zsh/configs/post/00-path.zsh
+++ zsh/configs/post/00-path.zsh
@@ -1,3 +1,7 @@
+if [[ -d "/usr/local/bin" && ! -z "${PATH##*/usr/local/bin*}" ]]; then
+  PATH="/usr/local/bin:$PATH"
+fi
+
 # ensure dotfiles bin directory is loaded first
 PATH="$HOME/bin:$HOME/.bin:$HOME/.asdf/bin:$HOME/.asdf/shims:/usr/local/sbin:$PATH"
 
