b02d5c2786a41b7ca166bf590677a5f78c50ebe4
Michael Herold <opensource@michaeljherold.com>
2020-10-30 11:58:15 -0500

    [emacs] Remove custom org-roam configuration
    Upstream added org-roam to the built-in org layer so I want to use that
    instead of relying on my own configuration.

--- tag-emacs/emacs.d/private/layers/mjh-org/packages.el
+++ tag-emacs/emacs.d/private/layers/mjh-org/packages.el
@@ -12,10 +12,6 @@
 (defconst mjh-org-packages
   '(
     org
-    org-roam
-    (company-org-roam
-     :location (recipe :fetcher github :repo "jethrokuan/company-org-roam")
-     :requires company)
     ))
 
 (defun mjh-org/post-init-org ()
@@ -27,42 +23,10 @@
                 1 'font-lock-org-checkbox-done prepend)))
   )
 
-(defun mjh-org/init-company-org-roam ()
-  (use-package company-org-roam
-    :defer t
-    :init
-    (spacemacs|add-company-backends
-      :backends company-org-roam
-      :modes org-roam-mode)))
-
-(defun mjh-org/init-org-roam ()
-  (use-package org-roam
-    :defer t
-    :hook (after-init . org-roam-mode)
-    :init
-    (progn
-      (spacemacs/declare-prefix "aon" "org-roam")
-      (spacemacs/set-leader-keys
-        "aonn" 'org-roam
-        "aonf" 'org-roam-find-file
-        "aong" 'org-roam-show-graph)
-
-      (spacemacs/declare-prefix-for-mode 'org-mode "n" "org-roam")
-      (spacemacs/set-leader-keys-for-major-mode 'org-mode
-        "nn" 'org-roam
-        "nt" 'org-journal-new-entry
-        "nb" 'org-roam-switch-to-buffer
-        "nf" 'org-roam-find-file
-        "ng" 'org-roam-show-graph
-        "ni" 'org-roam-insert)
-      )
-    )
-  )
-
 (with-eval-after-load 'org
   (add-to-list 'org-modules 'org-habit t)
 
   (evil-define-key* '(insert normal) org-mode-map
-                    (kbd "C-c n i") 'org-roam-insert
-                    (kbd "C-c n j") 'org-journal-new-entry)
+                    (kbd "C-c r i") 'org-roam-insert
+                    (kbd "C-c r j") 'org-journal-new-entry)
   )
