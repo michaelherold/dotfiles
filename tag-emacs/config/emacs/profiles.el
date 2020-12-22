a21a7abc99655efe127cf5ee837c905c46a599d5
Michael Herold <opensource@michaeljherold.com>
2020-12-22 16:59:04 -0600

    [emacs] Switch to using chemacs for profiles
    I'm interested in Doom Emacs, but it's not straightforward how to switch
    to Doom without clobbering my Spacemacs setup. Thus, I'm introducing
    chemacs, which will let me pick a different profile via a CLI flag.
    
    This change will require moving the `~/.config/emacs` directory that has
    Spacemacs in it to `~/.config/emacs-profiles/spacemacs`.

---
+++ tag-emacs/config/emacs/profiles.el
@@ -0,0 +1,4 @@
+(("default" . ((user-emacs-directory . "~/.config/emacs-profiles/spacemacs")
+               (server-name . "spacemacs")
+               (custom-file "~/.config/emacs-profiles/.cache/.custom-settings")
+               (env . (("SPACEMACSDIR" . "~/.config/spacemacs"))))))
