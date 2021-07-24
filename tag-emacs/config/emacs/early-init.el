f85906b81f07bc7e14d82d961806e8bf0b4c6dcc
Michael Herold <opensource@michaeljherold.com>
2021-07-23 22:43:12 -0500

    [emacs] Upgrade to chemacs 2
    chemacs 1 doesn't properly support `early-init.el` whereas chemacs
    2 does. This was frustrating when I was attempting to write my own
    custom configuration.
    
    I customized chemacs a little bit to remove some flexibility that
    I didn't need. Perhaps in the future I will pare it down even more!

--- tag-emacs/config/emacs/early-init.el
+++ tag-emacs/config/emacs/early-init.el
@@ -1,7 +1,7 @@
-;;; init.el --- -*- lexical-binding: t; -*-
+;;; early-init.el --- -*- lexical-binding: t; -*-
 
 (require 'chemacs
          (expand-file-name "chemacs.el"
                            (file-name-directory
                             (file-truename load-file-name))))
-(chemacs-load-user-init)
+(chemacs-load-user-early-init)
