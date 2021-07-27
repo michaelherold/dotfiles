2850e8ff8d14165fa48941ae6bcf9597450c03c6
Michael Herold <opensource@michaeljherold.com>
2021-07-27 10:46:26 -0500

    [zsh] Ensure bin folder is properly located
    This has been in a bug in my configuration forever and since I just
    fixed the "dottedness" of Library, I decided it was time to fix this
    one.

--- config/zsh/configs/post/00-path.zsh
+++ config/zsh/configs/post/00-path.zsh
@@ -3,7 +3,7 @@ if [[ -d "/usr/local/bin" && ! -z "${PATH##*/usr/local/bin*}" ]]; then
 fi
 
 # ensure dotfiles bin directory is loaded first
-PATH="$HOME/bin:$HOME/.bin:$HOME/.asdf/bin:$HOME/.asdf/shims:/usr/local/sbin:$PATH"
+PATH="$HOME/bin:$HOME/.asdf/bin:$HOME/.asdf/shims:/usr/local/sbin:$PATH"
 
 # mkdir .git/safe in the root of repositories you trust
 PATH=".git/safe/../../bin:$PATH"
