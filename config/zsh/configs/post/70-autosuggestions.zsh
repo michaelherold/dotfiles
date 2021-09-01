2cc8d7158878f5814d620d3e5175ed6a7519b89f
Michael Herold <opensource@michaeljherold.com>
2021-09-01 16:43:15 -0500

    [zsh] Add support for Homebrew zsh-autosuggestions
    I'm considering trying out a zsh plugin manager to preven this from
    being an issue, but I'm concerned it will bloat startup speeds too much.

--- config/zsh/configs/post/70-autosuggestions.zsh
+++ config/zsh/configs/post/70-autosuggestions.zsh
@@ -1,5 +1,6 @@
 #!/usr/bin/env zsh
 
+[[ "$(uname)" == 'Darwin' ]] && local directory=/usr/local/opt/zsh-autosuggestions/share/zsh-autosuggestions
 [[ "$(uname)" == 'Linux' ]] && local directory=/usr/share/zsh/plugins/zsh-autosuggestions
 
 local filename=${directory}/zsh-autosuggestions.zsh
