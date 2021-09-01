8ab06ee4f38feea9e4ce3128df500f1221b68475
Michael Herold <opensource@michaeljherold.com>
2021-08-31 22:15:44 -0500

    [tmux, vim, zsh] Enable true color support
    The base16-shell setup becomes unnecessary when we have true color
    support enabled in all command line tools. This set of changes:
    
    1. Tells tmux that any "256color" terminal supports true color (a
       dubious claim, but true for the terminal I care about)
    2. Tells vim to enable true color support, with a [a hack][1] of the
       [terminal information][2] within vim to support Alacritty's expected
       format; the hack does not apply to neovim
    3. Disables the integration with base16-shell in zsh because we do not
       need it any longer (being as I already configured Alacritty with the
       color scheme)
    
    [1]: https://github.com/tmux/tmux/issues/1246#issue-292083184
    [2]: https://github.com/vim/vim/blob/20e0c3d27bda770542c1c0e4c81fd6443c12f3a6/runtime/doc/term.txt#L548-L567

--- config/zsh/configs/color.zsh
+++ config/zsh/configs/color.zsh
@@ -4,11 +4,3 @@ colors
 
 # enable colored output from ls, etc. on FreeBSD-based systems
 export CLICOLOR=1
-
-# Enable base16 colorschemes when present
-BASE16_SHELL=$HOME/.config/base16-shell
-
-[[ -d "$BASE16_SHELL" ]] && \
-  [ -n "${PS1}" ] && \
-  [ -s ${BASE16_SHELL}/profile_helper.sh ] && \
-  eval "$(${BASE16_SHELL}/profile_helper.sh)"
