7f1d1129910b4e72db1c7a278aa1e2ad25611f65
Michael Herold <opensource@michaeljherold.com>
2021-09-10 23:10:17 -0500

    [git] Remove "safe bin" directories
    This was an interesting idea from thoughtbot's dotfiles but I never
    remember to use it. It also interferes when I'm building Arch packages
    so I decided to drop it.

--- config/zsh/configs/post/00-path.zsh
+++ config/zsh/configs/post/00-path.zsh
@@ -5,7 +5,4 @@ fi
 # ensure dotfiles bin directory is loaded first
 PATH="$HOME/bin:$HOME/.asdf/bin:$HOME/.asdf/shims:/usr/local/sbin:$PATH"
 
-# mkdir .git/safe in the root of repositories you trust
-PATH=".git/safe/../../bin:$PATH"
-
 export -U PATH
