c6e25edfc677ff4f55ee502dadfcc300400378d6
Michael Herold <opensource@michaeljherold.com>
2021-09-10 22:56:06 -0500

    [zsh] Remove unused zsh features
    Some of these I cargo-culted from the thoughtbot dotfiles repo. Because
    I didn't create them, I forget they are there. It's time to clean the
    house.
    
    Some of these are for old tools that I don't use anymore (e.g. pyenv and
    k8s).

--- config/zsh/configs/perl.zsh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-
-PATH="/home/herold/perl5/bin${PATH+:}${PATH}"; export PATH;
-PERL5LIB="/home/herold/perl5/lib/perl5${PERL5LIB+:}${PERL5LIB}"; export PERL5LIB;
-PERL_LOCAL_LIB_ROOT="/home/herold/perl5${PERL_LOCAL_LIB_ROOT+:}${PERL_LOCAL_LIB_ROOT}"; export PERL_LOCAL_LIB_ROOT;
-PERL_MB_OPT="--install_base \"/home/herold/perl5\""; export PERL_MB_OPT;
-PERL_MM_OPT="INSTALL_BASE=/home/herold/perl5"; export PERL_MM_OPT;
