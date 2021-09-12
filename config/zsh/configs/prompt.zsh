5b519beeaee0d7b1f84d223c6422833728f37484
Michael Herold <opensource@michaeljherold.com>
2021-09-11 19:35:03 -0500

    [zsh] Update Pure to latest and remove Starship
    I felt completely silly using a Rust program for my prompt so I decided
    to switch back after looking into options.

--- config/zsh/configs/prompt.zsh
+++ config/zsh/configs/prompt.zsh
@@ -2,13 +2,9 @@
 
 [ "$TERM" = "dumb" ] && return
 
-if command -v starship >/dev/null && [ -z "$DISABLE_STARSHIP" ]; then
-  eval "$(starship init zsh)"
-else
-  export PURE_PROMPT_SYMBOL="↪"
+export PURE_PROMPT_SYMBOL="↪"
 
-  fpath=("$ZDOTDIR/functions/prompt" $fpath)
+fpath=("$ZDOTDIR/functions/prompt" $fpath)
 
-  autoload -U promptinit && promptinit
-  prompt pure
-fi
+autoload -U promptinit && promptinit
+prompt pure
