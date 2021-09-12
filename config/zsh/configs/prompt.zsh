37b15b20e66988b1af89b42483d94ef54148b19a
Michael Herold <opensource@michaeljherold.com>
2021-09-12 14:17:38 -0500

    [zsh] Style new components of Pure prompt
    Flipping the arrow seems logical, but Fira's [right arrow curving
    left][1] is significantly different than its [left arrow curving
    right][2] so I might end up changing it.
    
    The dirty icon now matches the rest of the Git information. I don't like
    that information to pop, I like it to blend in.
    
    [1]: https://emojipedia.org/right-arrow-curving-left/
    [2]: https://emojipedia.org/left-arrow-curving-right/

--- config/zsh/configs/prompt.zsh
+++ config/zsh/configs/prompt.zsh
@@ -4,8 +4,11 @@
 
 export PURE_GIT_PULL=0
 export PURE_PROMPT_SYMBOL="↪"
+export PURE_PROMPT_VICMD_SYMBOL="↩️"
 
 fpath=("$ZDOTDIR/functions/prompt" $fpath)
+
+zstyle :prompt:pure:git:dirty color 242
 
 autoload -U promptinit && promptinit
 prompt pure
