9e03e239c2f304c95d658fa35f70f70c5cc24fa3
Michael Herold <opensource@michaeljherold.com>
2021-09-14 20:44:48 -0500

    [zsh] Tweak completion styles and settings
    These are largely based off [zimfw's completion module][1], but I read
    the documentation this time and understand it a lot more than my
    previous setup.
    
    [1]: https://github.com/zimfw/completion

--- config/zsh/configs/post/10-completion.zsh
+++ config/zsh/configs/post/10-completion.zsh
@@ -15,80 +15,55 @@ autoload -U compinit || exit
 
 compinit -C
 
-# Force rehash to have completion picking up new commands in path
-_force_rehash() { (( CURRENT == 1 )) && rehash; return 1 }
-zstyle ':completion:::::' completer _force_rehash \
-                                    _complete \
-                                    _ignored \
-                                    _gnu_generic \
-                                    _approximate
-zstyle ':completion:*'    completer _complete \
-                                    _ignored \
-                                    _gnu_generic \
-                                    _approximate
+# See zshcompsys(1) for the manual on these options
 
-# Speed up completion by avoiding partial globs
-zstyle ':completion:*' accept-exact '*(N)'
-zstyle ':completion:*' accept-exact-dirs true
+# Always use menu selection
+zstyle ':completion:*:*:*:*:*' menu select
 
-# Cache setup
-zstyle ':completion:*' use-cache on
+# Describe options, with the description if options aren't described
+zstyle ':completion:*:options' description yes
+zstyle ':completion:*:options' auto-description '%d'
 
-# Default colors for listings
-zstyle -e ':completion:*:default' list-colors 'reply=("${PREFIX:+=(#bi)($PREFIX:t)(?)*==02=01}:${(s.:.)LS_COLORS}")'
-
-# Separate directories from files
-zstyle ':completion:*' list-dirs-first true
-
-# Turn on menu selection only when selections do not fit on screen
-zstyle ':completion:*' menu true=long select=long
-
-# Separate matches into groups
-zstyle ':completion:*:matches' group yes
+# Separate matches into groups, using all types, with directories first
 zstyle ':completion:*' group-name ''
+zstyle ':completion:*' list-dirs-first true
+zstyle ':completion:*:matches' group yes
 
 # Always use the most verbose completion
 zstyle ':completion:*' verbose true
 
+# Try case-insensitive matching, then fuzzy matching
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' '+r:|?=**'
+
+# Presentation styles
+zstyle ':completion:*'              format '%F{cyan}-- %d --%f'
+zstyle ':completion:*:corrections'  format '%F{green}-- %d (errors: %e) --%f'
+zstyle ':completion:*:descriptions' format '%F{yellow}-- %d --%f'
+zstyle ':completion:*:messages'     format '%F{purple}-- %d --%f'
+zstyle ':completion:*:warnings'     format '%F{red}-- no matches found --%f'
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+
+# Cache setup, if any completions use it
+# See: _store_cache, _retrieve_cache, and _cache_invalid
+zstyle ':completion::complete:*' cache-path "${XDG_CACHE_HOME}/zsh/zcompcache"
+zstyle ':completion::complete:*' use-cache on
+
 # Treat sequences of slashes as single slash
 zstyle ':completion:*' squeeze-slashes true
 
-# Describe options.
-zstyle ':completion:*:options' description yes
+# Don't complete completion functions, widgets, or prompt commands
+zstyle ':completion:*:functions' ignored-patterns '(_*|pre(cmd|exec)|prompt_*)'
 
-# Completion presentation styles.
-zstyle ':completion:*:options' auto-description '%d'
-zstyle ':completion:*:descriptions' format $'\e[1m -- %d --\e[22m'
-zstyle ':completion:*:messages'     format $'\e[1m -- %d --\e[22m'
-zstyle ':completion:*:warnings'     format $'\e[1m -- No matches found --\e[22m'
-
-# Ignore hidden files by default
-zstyle ':completion:*:(all-|other-|)files'  ignored-patterns '*/.*'
-zstyle ':completion:*:(local-|)directories' ignored-patterns '*/.*'
-zstyle ':completion:*:cd:*'                 ignored-patterns '*/.*'
-
-# Don't complete completion functions/widgets.
-zstyle ':completion:*:functions' ignored-patterns '_*'
-
-
-# Show ignored patterns if needed.
-zstyle '*' single-ignored show
-
-# cd style.
-zstyle ':completion:*:cd:*' ignore-parents parent pwd # cd never selects the parent directory (e.g.: cd ../<TAB>)
-zstyle ':completion:*:*:cd:*' tag-order local-directories path-directories
-
-# kill style.
-zstyle ':completion:*:*:kill:*' command 'ps -a -w -w -u $USER -o pid,cmd --sort=-pid'
-zstyle ':completion:*:*:kill:*:processes' list-colors "=(#b) #([0-9]#)*=39=32"
-
-# rm/cp/mv style.
-zstyle ':completion:*:(rm|mv|cp):*' ignore-line yes
-
-# Hostnames completion.
-zstyle -e ':completion:*:hosts' hosts 'reply=(
-  ${${${${(f)"$(<~/.ssh/known_hosts)"}:#[\|]*}%%\ *}%%,*}
-  ${${${(@M)${(f)"$(<~/.ssh/config)"}:#Host *}#Host }:#*[*?]*}
-  ${(s: :)${(ps:\t:)${${(f)~~"$(</etc/hosts)"}%%\#*}#*[[:blank:]]}}
-)'
-zstyle ':completion:*:*:*:hosts' ignored-patterns 'ip6*' 'localhost*'
+# Command styles
+zstyle ':completion:*:*:cd:*' tag-order local-directories directory-stack path-directories
+zstyle ':completion:*:*:cd:*:directory-stack' menu yes select
+zstyle ':completion:*:history-words' stop yes
+zstyle ':completion:*:history-words' remove-all-dups yes
+zstyle ':completion:*:history-words' list false
+zstyle ':completion:*:history-words' menu yes
+zstyle ':completion:*:manuals' separate-sections true
+zstyle ':completion:*:manuals.(^1*)' insert-sections true
+zstyle ':completion:*:(diff|kill|rm):*' ignore-line other
+zstyle ':completion:*:rm:*' file-patterns '*:all-files'
+zstyle ':completion:*:-tilde-:*' group-order named-directories path-directories expand
+zstyle ':completion:*:*:-subscript-:*' tag-order indexes parameters
