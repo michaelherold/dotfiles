f939d3e4f3a72b2e6e529c43c679e13ef9b21cd4
Michael Herold <opensource@michaeljherold.com>
2021-12-17 09:22:00 -0600

    [gpg] Silence prompting for agent connection
    The version that my development environment installs on my work machine
    is chatty. This makes it so it's less so.

--- config/zsh/configs/gpg.zsh
+++ config/zsh/configs/gpg.zsh
@@ -3,4 +3,4 @@
 export GPG_TTY=$(tty)
 export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
 
-gpg-connect-agent updatestartuptty /bye >/dev/null
+gpg-connect-agent updatestartuptty /bye >/dev/null 2>&1
