a7d24770a2900da9389b25205e3357cf4ba2fd90
Michael Herold <opensource@michaeljherold.com>
2023-11-15 22:05:12 -0600

    [emacs] Clean up old, unused profiles
    I've been on Doom long enough that it's safe to say that I will never go
    back to Spacemacs or my old configuration. It's time to get rid of them!

--- tag-emacs/config/emacs/profiles.el
+++ tag-emacs/config/emacs/profiles.el
@@ -1,7 +1,3 @@
 ;;; ~/.config/emacs/profiles.el -*- lexical-binding: t; -*-
 
-(("default"   . ((user-emacs-directory . "~/.config/emacs/profiles/doom")))
- ("spacemacs" . ((user-emacs-directory . "~/.config/emacs/profiles/spacemacs")
-                 (server-name . "spacemacs")
-                 (custom-file "~/.config/emacs/profiles/spacemacs/.cache/.custom-settings")
-                 (env . (("SPACEMACSDIR" . "~/.config/spacemacs"))))))
+(("default"   . ((user-emacs-directory . "~/.config/emacs/profiles/doom"))))
